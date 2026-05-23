70a8ebebfedc85ce221667ef74cdd23c72fa38c5
Firedrago95 <yonghwa95@naver.com>
2026-05-23 15:45:00 +0900

    fix: MockExternalNotificationClient에 @Component 추가

--- src/main/java/com/liveclass/notification/infrastructure/external/MockExternalNotificationClient.java
+++ src/main/java/com/liveclass/notification/infrastructure/external/MockExternalNotificationClient.java
@@ -1,8 +1,10 @@
 package com.liveclass.notification.infrastructure.external;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class MockExternalNotificationClient implements ExternalNotificationClient{
 
     @Override
